d0a81d008b131bbe2a4a7b45567a0feec22dd556
jdabrams2000 <abramsj@bxscience.edu>
2018-02-25 17:28:31 -0500

    Add files via upload
    VR Auto Walk

---
+++ VR_CP/VR_AutoWalk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_AutoWalk : MonoBehaviour {
+
+	public float speed = 5.0f;			//speed
+	public bool moveForward;			//Move or Not
+	private CharacterController cc;		//Character Controlled Script
+	private GvrEditorEmulator gvrEditor;
+	private Transform vrHead;
+
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+		gvrEditor = transform.GetChild (0).GetComponent<GvrEditorEmulator> ();
+		vrHead = Camera.main.transform;
+	}
+
+	void Update() {
+		if (Input.GetButtonDown ("Fire1")) {
+			moveForward = !moveForward;
+		}
+
+		if (moveForward) {
+			Vector3 forward = vrHead.TransformDirection (Vector3.forward);
+			cc.SimpleMove (forward * speed);
+		}
+	}
+}
