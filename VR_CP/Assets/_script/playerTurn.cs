018b71a626b9986d73e1cf0ec55535857ab206d8
Andy Zheng <zhenga6@bxscience.edu>
2018-03-04 18:22:43 -0500

    TS1 player
    PLayer is able to rotate based on camera movement.

---
+++ VR_CP/Assets/_script/playerTurn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerTurn : MonoBehaviour {
+
+	public GameObject cam;
+
+	// Update is called once per frame
+	void Update () {
+		Quaternion charRot = cam.transform.rotation;
+		charRot.x = 0;
+		charRot.y = cam.transform.rotation.y;
+		charRot.z = 0;
+		this.transform.rotation = charRot * Quaternion.Euler(0,90,0);
+	}
+}
