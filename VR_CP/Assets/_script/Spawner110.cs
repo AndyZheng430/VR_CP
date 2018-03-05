c63c69bb434451dc954aca9c389bc4e03e6e388f
Andy Zheng <zhenga6@bxscience.edu>
2018-03-04 23:45:50 -0500

    TS1 complete for single player
    snow is working
    random spawn obstacles working
    auto walk working

--- VR_CP/Assets/_script/Spawner110.cs
+++ VR_CP/Assets/_script/Spawner110.cs
@@ -7,7 +7,8 @@ public class Spawner110 : MonoBehaviour {
 	public GameObject[] obs;
 	//z=485, x=35, z=335, x=135
 	public int Xbeg = 30;
-	public int Xend = 140; 
+	public int Xend = 140;
+	public int Ypole = 110;
 	public int Zbeg = 330;
 	public int Zend = 490;
 	public int size = 100;
@@ -17,7 +18,7 @@ public class Spawner110 : MonoBehaviour {
 	}
 	IEnumerator spawning(){
 		while (size >= 0) {
-			Instantiate (obs [(int)(Random.Range (0, obs.Length))], new Vector3 ((int)(Random.Range (Xbeg, Xend)), 110, (int)(Random.Range (Zbeg, Zend))), Quaternion.identity);
+			Instantiate (obs [(int)(Random.Range (0, obs.Length))], new Vector3 ((int)(Random.Range (Xbeg, Xend)), Ypole, (int)(Random.Range (Zbeg, Zend))), Quaternion.identity);
 			size = size - 1;
 		}
 		yield return new WaitForSeconds (1);
